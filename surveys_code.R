31a276f2915b62a8910b5e5a4356e2a261e5c135
Matt Bixley <mattbixley72@gmail.com>
2020-06-09 09:16:28 +1200

    surveys files

---
+++ surveys_code.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+download.file(url="https://ndownloader.figshare.com/files/2292169",
+              destfile = "portal_data_joined.csv")
+surveys <- read.csv("portal_data_joined.csv")
+
+surveys_complete <- surveys %>%
+  filter(!is.na(weight),           # remove missing weight
+         !is.na(hindfoot_length),  # remove missing hindfoot_length
+         !is.na(sex))                # remove missing sex
+
+write_csv(surveys_complete, path = "surveys_complete.csv")
