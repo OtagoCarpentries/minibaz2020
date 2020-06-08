e91cb09545515d767fd604c77965758f7d08e5c6
Matt Bixley <mattbixley72@gmail.com>
2020-06-09 10:45:19 +1200

    Update surveys_code.R

--- surveys_code.R
+++ surveys_code.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 download.file(url="https://ndownloader.figshare.com/files/2292169",
               destfile = "portal_data_joined.csv")
-surveys <- read.csv("portal_data_joined.csv")
+surveys <- read_csv("portal_data_joined.csv")
 
 surveys_complete <- surveys %>%
   filter(!is.na(weight),           # remove missing weight
